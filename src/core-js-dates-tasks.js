2d65529f9bc66a73098656bf08e129c534d2bc34
khvorosttt <khvorosttt@yandex.by>
2024-03-06 01:53:16 +0300

    add implementation method getTime

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -31,8 +31,16 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+
+function numberFormat(value) {
+  return value < 10 ? `0${value}` : value;
+}
+
+function getTime(date) {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  return `${numberFormat(hours)}:${numberFormat(minutes)}:${numberFormat(seconds)}`;
 }
 
 /**
